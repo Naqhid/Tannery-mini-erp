e13ad070497a7639cd043b6324e154629836dbe4
Mohammed Naqhid <mnaqhid@gmail.com>
2026-06-28 10:47:25 +0530

    Add GitHub Pages deployment workflow

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # tannery-erp-mini
 
-[![Open in Bolt](https://bolt.new/static/open-in-bolt.svg)](https://bolt.new/~/sb1-cf5futzj)
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Tannery-mini-erp/',
   plugins: [react()],
   optimizeDeps: {
     include: ['lucide-react'],
